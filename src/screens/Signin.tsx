fe1cf3d4e1014305c6e0db6ab146c40bdc00bbe6
MateusFCD <mateusdrumond.dev@gmail.com>
2023-04-17 09:08:10 -0300

    update

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,15 @@
+import { VStack, Image } from "native-base";
+import BackgorundImg from "@assets/background.png";
+
+export function SignIn() {
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Image
+        source={BackgorundImg}
+        alt=""
+        resizeMode="contain"
+        position="absolute"
+      />
+    </VStack>
+  );
+}
